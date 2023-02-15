a23f869208b8c25c8ef0c92621026205fd45b899
Thomas-Vargas <tavargas2012irc@gmail.com>
2023-02-14 23:08:45 -0600

    Altered search function to include trackName and tested for all scenarios involving multiple or missing arguments.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,13 +10,6 @@ function addToCollection( title, artist, yearPublished, tracksProvided) {
         tracks: tracksProvided
     }
 
-    /*for (let i = 0; i < tracksProvided.length; i++) {
-        album.tracks.push({
-            name: tracks[i].name,
-            duration: tracks[i].duration
-        })
-    } */
-
     collection.push(album);
     return album;
 }
@@ -49,12 +42,11 @@ function showCollection( albums ) {
     console.log(`Total of ${albums.length} albums in collection:`);
 
     for (let i = 0; i < albums.length; i++) {
-        console.log(`${albums[i].title} by ${albums[i].artist}, published in year ${albums[i].year}:`)
+        console.log(`${albums[i].title} by ${albums[i].artist}, published in year ${albums[i].year}:`);
         
         if (albums[i].tracks !== undefined) {
             for (let z = 0; z < albums[i].tracks.length; z++) {
-            
-                console.log(`${z+1}. ${albums[i].tracks[z].name}: ${albums[i].tracks[z].duration}`)
+                console.log(`${z+1}. ${albums[i].tracks[z].name}: ${albums[i].tracks[z].duration}`);
             }
         }
     }
@@ -83,20 +75,44 @@ console.log('Should return empty array' ,findByArtist('Slipknot'));
 //Test - test for multiple matches 
 console.log('Should return two matches:', findByArtist('Gregory Alan Isakov'));
 
-function search( object ){
+function search( object, trackName ){
     let match = [];
+    let song = trackName;
 
-    if (object === undefined || Object.keys(object).length === 0) {
-        return collection;
+    //If only one argument provided, store trackName in variable song
+    if (arguments.length === 1 && typeof arguments[0] === 'string') {
+        song = arguments[0];
     }
 
+    //Return entire collection if no object or trackname, or if empty object is provided,
+    if (object === undefined || Object.keys(object).length === 0) {
+        if (trackName === undefined) {
+            return collection;
+        }
+    }
+
+    //If trackName is provided, or if only given trackName, prioritize trackName and search for match
+    if (arguments[1] !== undefined || typeof arguments[0] === 'string') {
+        for (let i = 0; i < collection.length; i++) {   
+            if (collection[i].tracks !== undefined) {
+                for (let z = 0; z < collection[i].tracks.length; z++) {
+                    if (collection[i].tracks[z].name === song) {
+                        match.push(collection[i]);
+                        return match;
+                    }
+                }
+            }
+        }
+    } 
+
+    //Otherwise look for match based on artist and year
     for (let i = 0; i < collection.length; i++) {
         if (collection[i].artist === object.artist && collection[i].year === object.year) {
             match.push(collection[i])
         }
     }
-    
-    return match
+    //Return any matches or empty array if no matches
+    return match;  
 }
 
 // Test - test for no match, should return empty array
@@ -106,7 +122,19 @@ console.log('should return empty array:', search({artist: 'Ray Charles', year: 1
 console.log('Should return entire collection:', search());
 
 //Test - test for empty object provided
-console.log('Should return entire collection:', search([]));
+console.log('Should return entire collection:', search({}));
 
-//Test - test for match
+//Test - test for match given object with artist and year
 console.log('should return one match:', search({artist: 'Gregory Alan Isakov', year: 2007}));
+
+//Test - test search with trackName and empty object
+console.log('Should return object info:', search({} ,'Around the Fur'));
+
+//Test - test search with only trackName
+console.log('Should return object info:', search('The Stable Song'));
+
+//Test - test search when given both object and trackname
+console.log('Should prioritize trackname, returning Gregory Alan Isakov object:', search({artist: 'Lucy Dacus', year: 2018}, 'The Stable Song'));
+
+//Test - test search when given nonexistent trackName but existing object
+console.log('Should return Lucy Dacus object', search({artist: 'Lucy Dacus', year: 2018}, 'These Days'));
